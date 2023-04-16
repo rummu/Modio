929350d469209eff4f4fb1475d7c5549ee0d16d3
Rumman Ahmad <rummanahmad05@gmail.com>
2023-04-16 15:21:13 +0530

    First

--- camera.py
+++ camera.py
@@ -1,7 +1,19 @@
 import cv2
-face_cascade=cv2.CascadeClassifier("haarcascade_frontalface_alt2.xml")
-ds_factor=0.6
-
+import av
+import numpy as np 
+import mediapipe as mp 
+from keras.models import load_model 
+import webbrowser
+model  = load_model("model.h5")
+label = np.load("labels.npy")
+holistic = mp.solutions.holistic
+hands = mp.solutions.hands
+holis = holistic.Holistic()
+drawing = mp.solutions.drawing_utils
+try:
+     emotion=np.load("emotion.npy")[0]
+except:
+      emotion=""
 class VideoCamera(object):
     def __init__(self):
         self.video = cv2.VideoCapture(0)
@@ -10,12 +22,108 @@ class VideoCamera(object):
         self.video.release()
     
     def get_frame(self):
-        success, image = self.video.read()
-        image=cv2.resize(image,None,fx=ds_factor,fy=ds_factor,interpolation=cv2.INTER_AREA)
-        gray=cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
-        face_rects=face_cascade.detectMultiScale(gray,1.3,5)
-        for (x,y,w,h) in face_rects:
-        	cv2.rectangle(image,(x,y),(x+w,y+h),(0,255,0),2)
-        	break
-        ret, jpeg = cv2.imencode('.jpg', image)
-        return jpeg.tobytes()
+        success, frm = self.video.read()
+        #start
+        frm = cv2.flip(frm, 1)
+
+        res = holis.process(cv2.cvtColor(frm, cv2.COLOR_BGR2RGB))
+        lst=[]
+        global em
+
+        if res.face_landmarks:
+            for i in res.face_landmarks.landmark:
+                lst.append(i.x - res.face_landmarks.landmark[1].x)
+                lst.append(i.y - res.face_landmarks.landmark[1].y)
+
+            if res.left_hand_landmarks:
+                for i in res.left_hand_landmarks.landmark:
+                    lst.append(i.x - res.left_hand_landmarks.landmark[8].x)
+                    lst.append(i.y - res.left_hand_landmarks.landmark[8].y)
+            else:
+                for i in range(42):
+                    lst.append(0.0)
+
+            if res.right_hand_landmarks:
+                for i in res.right_hand_landmarks.landmark:
+                    lst.append(i.x - res.right_hand_landmarks.landmark[8].x)
+                    lst.append(i.y - res.right_hand_landmarks.landmark[8].y)
+            else:
+                for i in range(42):
+                    lst.append(0.0)
+
+            lst = np.array(lst).reshape(1,-1)
+
+            pred = label[np.argmax(model.predict(lst))]
+            em=pred
+
+            # print(pred)
+            cv2.putText(frm, pred, (50,50),cv2.FONT_ITALIC, 1, (255,0,0),2)
+            np.save("emotion.npy",np.array([pred]))
+            
+           # drawing.draw_landmarks(frm, res.face_landmarks, holistic.FACEMESH_CONTOURS)
+          #  drawing.draw_landmarks(frm, res.left_hand_landmarks, hands.HAND_CONNECTIONS)
+           # drawing.draw_landmarks(frm, res.right_hand_landmarks, hands.HAND_CONNECTIONS)
+
+
+        #end
+
+        # return av.VideoFrame.from_ndarray(frm,format="bgr24")
+        # image=cv2.resize(image,None,fx=ds_factor,fy=ds_factor,interpolation=cv2.INTER_AREA)
+        # gray=cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+        # face_rects=face_cascade.detectMultiScale(gray,1.3,5)
+        # for (x,y,w,h) in face_rects:
+        # 	cv2.rectangle(image,(x,y),(x+w,y+h),(0,255,0),2)
+        # 	break
+        ret, jpeg = cv2.imencode('.jpg', frm)
+        return jpeg.tobytes(),em
+# class VideoCamera(object):
+#     def recv(self,frame):
+#         frm=frame.to_ndarray(format="bgr24")
+
+        # #start
+        # frm = cv2.flip(frm, 1)
+
+        # res = holis.process(cv2.cvtColor(frm, cv2.COLOR_BGR2RGB))
+        # lst=[]
+
+        # if res.face_landmarks:
+        #     for i in res.face_landmarks.landmark:
+        #         lst.append(i.x - res.face_landmarks.landmark[1].x)
+        #         lst.append(i.y - res.face_landmarks.landmark[1].y)
+
+        #     if res.left_hand_landmarks:
+        #         for i in res.left_hand_landmarks.landmark:
+        #             lst.append(i.x - res.left_hand_landmarks.landmark[8].x)
+        #             lst.append(i.y - res.left_hand_landmarks.landmark[8].y)
+        #     else:
+        #         for i in range(42):
+        #             lst.append(0.0)
+
+        #     if res.right_hand_landmarks:
+        #         for i in res.right_hand_landmarks.landmark:
+        #             lst.append(i.x - res.right_hand_landmarks.landmark[8].x)
+        #             lst.append(i.y - res.right_hand_landmarks.landmark[8].y)
+        #     else:
+        #         for i in range(42):
+        #             lst.append(0.0)
+
+        #     lst = np.array(lst).reshape(1,-1)
+
+    #pred = label[np.argmax(model.predict(lst))]
+
+        #     # print(pred)
+        #     cv2.putText(frm, pred, (50,50),cv2.FONT_ITALIC, 1, (255,0,0),2)
+
+            
+        # drawing.draw_landmarks(frm, res.face_landmarks, holistic.FACEMESH_CONTOURS)
+        # drawing.draw_landmarks(frm, res.left_hand_landmarks, hands.HAND_CONNECTIONS)
+        # drawing.draw_landmarks(frm, res.right_hand_landmarks, hands.HAND_CONNECTIONS)
+        # #end
+        # return av.VideoFrame.from_ndarray(frm,format="bgr24")
+
+#btn=st.button("Recommend me songs")
+#if btn:
+   # if not(emotion):
+       # print("please")
+    #else:
+        #webbrowser.open(f"https://www.youtube.com/results?search_query={emotion}+songs")
--- main.py
+++ main.py
@@ -1,23 +1,42 @@
-
-from flask import Flask, render_template, Response
+from flask import Flask, render_template, Response,jsonify 
 from camera import VideoCamera
+import numpy as np 
 
+
+
+# app = Flask(__name__)
 app = Flask(__name__)
+
+emotion=''
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index2.html',emotion={emotion})
 
 def gen(camera):
     while True:
-        frame = camera.get_frame()
+        global emotion
+        frame,emotion = camera.get_frame()
+        # print(emotion)
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 
 @app.route('/video_feed')
 def video_feed():
+    # x,emotion=VideoCamera() 
+    # print(emotion)
     return Response(gen(VideoCamera()),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
+    # return Response(gen(x),
+    #                 mimetype='multipart/x-mixed-replace; boundary=frame')
+
+@app.route('/t')
+def get_emotion():
+    data={
+        "Emotion":emotion
+    }
+    return jsonify(data)
+
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    app.run()
